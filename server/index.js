c58b9d0ab82cdb6122181935d6e131cd74957918
AnujTanwar2004 <anujtanwar2414@gmail.com>
2026-04-20 08:34:13 +0530

    she is so dope

--- server/index.js
+++ server/index.js
@@ -4,14 +4,16 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const distPath = path.join(__dirname, "../dist");
 
 app.use(cors());
 app.use(express.json());
 app.use("/resume", express.static(path.join(__dirname, "../public")));
+app.use(express.static(distPath));
 
 app.post("/api/contact", (req, res) => {
   const { name, email, message } = req.body;
@@ -29,6 +31,10 @@ app.get("/api/health", (_, res) => {
   res.json({ ok: true, status: "server-running" });
 });
 
+app.get("/{*splat}", (_, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`Portfolio backend running at http://localhost:${PORT}`);
+  console.log(`Portfolio app running on port ${PORT}`);
 });
